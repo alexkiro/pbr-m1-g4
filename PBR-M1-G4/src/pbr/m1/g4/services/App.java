b24f7cae7c442f955fbcb0fdbad7e04fe64248de
cmunteanu <cmunteanu@cf.munteanu@gmail.com>
2013-05-13 11:26:36 +0300

    Added main application class App.java

---
+++ PBR-M1-G4/src/pbr/m1/g4/services/App.java
@@ -0,0 +1,14 @@
+package pbr.m1.g4.services;
+
+/**
+ * Application main class
+ * @author Echipa 4 PBR 2013
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        
+        // Test parse and generate xml
+        XMLFactory.init("input.xml");
+    }    
+}
